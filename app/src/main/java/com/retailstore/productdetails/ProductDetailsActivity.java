7741ecbe4dc35de63decd51d92df9a99bf651ab4
sameer.belsare <sameer.belsare@gmail.com>
2017-02-14 13:39:51 +0530

    Added databinding on product details screen

--- app/src/main/java/com/retailstore/productdetails/ProductDetailsActivity.java
+++ app/src/main/java/com/retailstore/productdetails/ProductDetailsActivity.java
@@ -1,22 +1,19 @@
 package com.retailstore.productdetails;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.retailstore.Constants;
 import com.retailstore.Product;
 import com.retailstore.R;
 import com.retailstore.cart.CartActivity;
+import com.retailstore.databinding.ActivityProductDetailsBinding;
 
 /**
  * Created by sameer.belsare on 13/2/17.
@@ -25,14 +22,15 @@ import com.retailstore.cart.CartActivity;
 public class ProductDetailsActivity extends AppCompatActivity implements ProductDetailsView, View.OnClickListener {
     private ProductDetailsPresenterImpl productDetailsPresenter;
     private Product product;
+    private ActivityProductDetailsBinding activityProductDetailsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_details);
+        activityProductDetailsBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_product_details);
         int id = getIntent().getIntExtra("id", 0);
-        Button button = (Button) findViewById(R.id.btnCart);
-        button.setOnClickListener(this);
+        activityProductDetailsBinding.btnCart.setOnClickListener(this);
         productDetailsPresenter = new ProductDetailsPresenterImpl(this, id);
         productDetailsPresenter.getProductDetails();
     }
@@ -46,13 +44,9 @@ public class ProductDetailsActivity extends AppCompatActivity implements Product
     @Override
     public void showProductDetails(Product product) {
         this.product = product;
-        ImageView imageView = (ImageView) findViewById(R.id.ivImage);
+        activityProductDetailsBinding.setProduct(product);
         int resID = getResources().getIdentifier(product.getImage(), "drawable",  getPackageName());
-        Glide.with(this).load(resID).placeholder(R.mipmap.ic_launcher).into(imageView);
-        ((TextView) findViewById(R.id.name)).setText(product.getName());
-        ((TextView) findViewById(R.id.category)).setText(getString(R.string.category_string) + Constants.PRODUCT_CATEGORY.values()[product.getCategory()].name());
-        ((TextView) findViewById(R.id.price)).setText(getString(R.string.price)+getString(R.string.rupee)+product.getPrice());
-        ((TextView) findViewById(R.id.details)).setText(getString(R.string.description)+product.getDetails());
+        Glide.with(this).load(resID).placeholder(R.mipmap.ic_launcher).into(activityProductDetailsBinding.ivImage);
     }
 
     @Override
