6bdb22714880116fed61008b6ecd6f9b901f8f13
sameer.belsare <sameer.belsare@gmail.com>
2017-02-14 15:31:59 +0530

    Added databinding on cart screen and its recycler view adapter

--- app/src/main/java/com/retailstore/cart/CartActivity.java
+++ app/src/main/java/com/retailstore/cart/CartActivity.java
@@ -1,17 +1,16 @@
 package com.retailstore.cart;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.retailstore.Product;
 import com.retailstore.R;
+import com.retailstore.databinding.ActivityCartBinding;
 import com.retailstore.productdetails.ProductDetailsActivity;
 
 import java.util.List;
@@ -21,16 +20,13 @@ import java.util.List;
  */
 public class CartActivity extends AppCompatActivity implements CartView, View.OnClickListener {
     private CartPresenter cartPresenter;
-    private TextView noProductsInCartText;
-    private RecyclerView listView;
     private CartAdapter cartAdapter;
+    private ActivityCartBinding activityCartBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
-        listView = (RecyclerView) findViewById(R.id.cartList);
-        noProductsInCartText = (TextView) findViewById(R.id.noProductsInCartText);
+        activityCartBinding = DataBindingUtil.setContentView(this, R.layout.activity_cart);
         cartPresenter = new CartPresenterImpl(this);
         cartPresenter.getItemsInCart();
     }
@@ -44,12 +40,12 @@ public class CartActivity extends AppCompatActivity implements CartView, View.On
     @Override
     public void loadCartData(List<Product> cartProducts) {
         if(cartProducts != null && cartProducts.size() > 0) {
-            noProductsInCartText.setVisibility(View.GONE);
+            activityCartBinding.noProductsInCartText.setVisibility(View.GONE);
             cartAdapter = new CartAdapter(cartProducts, this, this);
-            listView.setAdapter(cartAdapter);
+            activityCartBinding.cartList.setAdapter(cartAdapter);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-            listView.setLayoutManager(linearLayoutManager);
-            listView.setHasFixedSize(true);
+            activityCartBinding.cartList.setLayoutManager(linearLayoutManager);
+            activityCartBinding.cartList.setHasFixedSize(true);
             cartPresenter.getTotalAmount();
         }
     }
@@ -63,9 +59,11 @@ public class CartActivity extends AppCompatActivity implements CartView, View.On
 
     @Override
     public void setTotalAmount(int totalItems, int amount) {
-        ((TextView) findViewById(R.id.priceText)).setText("Price ("+totalItems+" item/s)");
-        ((TextView) findViewById(R.id.totalPrice)).setText(getString(R.string.rupee)+amount);
-        ((TextView) findViewById(R.id.totalAmount)).setText(getString(R.string.rupee)+amount+getString(R.string.dash_slash));
+        if(amount == 0){
+            activityCartBinding.noProductsInCartText.setVisibility(View.VISIBLE);
+        }
+        activityCartBinding.setTotalItems(" ("+totalItems+" item/s)");
+        activityCartBinding.setAmount(getString(R.string.rupee)+amount+getString(R.string.dash_slash));
     }
 
     @Override
--- app/src/main/java/com/retailstore/cart/CartAdapter.java
+++ app/src/main/java/com/retailstore/cart/CartAdapter.java
@@ -1,19 +1,16 @@
 package com.retailstore.cart;
 
 import android.content.Context;
-import android.net.Uri;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.retailstore.Constants;
 import com.retailstore.Product;
 import com.retailstore.R;
+import com.retailstore.databinding.CartItemBinding;
 
 import java.util.List;
 
@@ -35,24 +32,22 @@ public class CartAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.cart_item, parent,
-                false);
-        view.setOnClickListener(mItemClickListener);
-        return new CartListViewHolder(view);
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        CartItemBinding cartItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.cart_item, parent, false);
+        cartItemBinding.getRoot().setOnClickListener(mItemClickListener);
+        return new CartListViewHolder(cartItemBinding);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
         CartListViewHolder viewHolder = (CartListViewHolder) holder;
         Product product = products.get(position);
+        viewHolder.bind(product);
         int resID = mContext.getResources().getIdentifier(product.getImage(), "drawable",  mContext.getPackageName());
-        Glide.with(mContext).load(resID).asBitmap().placeholder(R.mipmap.ic_launcher).into(viewHolder.image);
-        viewHolder.name.setText(product.getName());
-        viewHolder.category.setText(mContext.getString(R.string.category_string) + Constants.PRODUCT_CATEGORY.values()[product.getCategory()].name());
-        viewHolder.price.setText(mContext.getString(R.string.rupee)+product.getPrice());
+        Glide.with(mContext).load(resID).asBitmap().placeholder(R.mipmap.ic_launcher).into(viewHolder.cartItemBinding.image);
         viewHolder.itemView.setTag(product.getId());
-        viewHolder.delete.setOnClickListener(mItemClickListener);
-        viewHolder.delete.setTag(product);
+        viewHolder.cartItemBinding.delete.setOnClickListener(mItemClickListener);
+        viewHolder.cartItemBinding.delete.setTag(product);
     }
 
     @Override
@@ -61,19 +56,16 @@ public class CartAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     }
 
     private static class CartListViewHolder extends RecyclerView.ViewHolder {
-        public ImageView image;
-        public TextView name;
-        public TextView price;
-        public TextView category;
-        public Button delete;
+        private final CartItemBinding cartItemBinding;
 
-        public CartListViewHolder(View itemView) {
-            super(itemView);
-            image = (ImageView) itemView.findViewById(R.id.image);
-            name = (TextView) itemView.findViewById(R.id.name);
-            price = (TextView) itemView.findViewById(R.id.price);
-            category = (TextView) itemView.findViewById(R.id.category);
-            delete = (Button) itemView.findViewById(R.id.delete);
+        public CartListViewHolder(CartItemBinding cartItemBinding) {
+            super(cartItemBinding.getRoot());
+            this.cartItemBinding = cartItemBinding;
+        }
+
+        public void bind(Product product) {
+            cartItemBinding.setProduct(product);
+            cartItemBinding.executePendingBindings();
         }
     }
 }
